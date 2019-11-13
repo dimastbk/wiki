d9746b2c5f41b2bd54061beaa22beb4a7d9fe55d
Дмитрий Стародубов <dmitrii.starodubov@fogstream.ru>
2019-11-13 18:25:27 +1000

    + ОКТМО, +координаты

--- apps/gkgn/models.py
+++ apps/gkgn/models.py
@@ -5,9 +5,16 @@ class Settlement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     gkgn_id = db.Column(db.Integer)
     name = db.Column(db.String(256))
+    okato_name = db.Column(db.String(256))
+    oktmo_name = db.Column(db.String(256))
     types = db.Column(db.String(100))
     region = db.Column(db.String(256))
     district = db.Column(db.String(256))
+    c_lat = db.Column(db.Float(precision=6))
+    c_lon = db.Column(db.Float(precision=6))
+    okato = db.Column(db.String(11))
+    oktmo = db.Column(db.String(11))
+    oktmo_up = db.Column(db.String(11))
 
     def __repr__(self):
         return '<Settlement {}>'.format(self.name)
@@ -17,8 +24,13 @@ class ATE(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     gkgn_id = db.Column(db.Integer)
     name = db.Column(db.String(256))
+    okato_name = db.Column(db.String(256))
+    oktmo_name = db.Column(db.String(256))
     types = db.Column(db.String(100))
     region = db.Column(db.String(256))
+    okato = db.Column(db.String(11))
+    oktmo = db.Column(db.String(11))
+    oktmo_up = db.Column(db.String(11))
 
     def __repr__(self):
         return '<ATE {}>'.format(self.name)
