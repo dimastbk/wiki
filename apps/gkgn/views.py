1245c1ca372ff13693e24b9b5918acba924999a0
dimastbk <dimastbk@yandex.ru>
2022-01-29 17:37:13 +1000

    + type of object in gkgn, + select2 for type/region/district

--- apps/gkgn/views.py
+++ apps/gkgn/views.py
@@ -4,7 +4,7 @@ from typing import Any
 from flask import Blueprint, render_template, request
 
 from .constants import LevelEnum
-from .models import Object
+from .models import Object, Type
 
 gkgn_bp = Blueprint("gkgn", __name__)
 
@@ -29,8 +29,9 @@ def index_gkgn():
 
     form["gkgn_id"] = request.values.get("gkgn_id", "").lstrip("0")
     form["name"] = request.values.get("name", "")
-    form["district_id"] = to_int(request.values.get("district_id"))
     form["region_id"] = to_int(request.values.get("region_id"))
+    form["district_id"] = to_int(request.values.get("district_id"))
+    form["type_id"] = to_int(request.values.get("type_id"))
 
     regions = tuple(
         Object.query.filter_by(level=LevelEnum.REGION.value)
@@ -38,12 +39,18 @@ def index_gkgn():
         .values(Object.id, Object.name)
     )
 
-    region_filter = {"region_id": form["region_id"]} if form["region_id"] else {}
-    districts = tuple(
-        Object.query.filter_by(level=LevelEnum.DISTRICT.value, **region_filter)
-        .order_by(Object.name)
-        .values(Object.id, Object.name)
-    )
+    if form["region_id"]:
+        districts = tuple(
+            Object.query.filter_by(
+                level=LevelEnum.DISTRICT.value, region_id=form["region_id"]
+            )
+            .order_by(Object.name)
+            .values(Object.id, Object.name)
+        )
+    else:
+        districts = []
+
+    types = tuple(Type.query.order_by(Type.name).values(Type.id, Type.name))
 
     query = {key: value for key, value in form.items() if value}
     if query:
@@ -53,6 +60,7 @@ def index_gkgn():
         "gkgn/index.html",
         regions=regions,
         districts=districts,
+        types=types,
         result=result,
         len=len(result),
         time=default_timer() - t,
